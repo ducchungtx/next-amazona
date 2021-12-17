45a6dcfac34381ee00f934164c25ac40de91253c
Chung Nguyen <zrmedia9@gmail.com>
2021-12-17 21:37:20 +0700

    Done for get API Product

--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,17 @@ import {
 import NextLink from 'next/link';
 
 import Layout from '../components/Layout';
-import data from '../utils/data';
+import Product from '../models/Product';
+import db from '../utils/db';
 
-export default function Home() {
+export default function Home(props) {
+  const {products} = props;
   return (
     <Layout>
       <div>
       <h1>Products</h1>
       <Grid container spacing={3}>
-        { data.products.map((product) => (
+        { products.map((product) => (
           <Grid item key={product.name} md={4}>
             <Card>
               <NextLink href={`/product/${product.slug}`} passHref>
@@ -41,4 +43,15 @@ export default function Home() {
     </div>
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find().lean();
+  await db.disconnect();
+  return {
+    props: {
+      products: products.map(db.convertDocToObj)
+    }
+  }
 }
