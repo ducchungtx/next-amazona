78f1afea5719f49db84135b2fd2a5b8eb009b747
Chung Nguyen <zrmedia9@gmail.com>
2022-01-06 15:49:04 +0700

    Update the code for main admin dashboard page

---
+++ pages/admin/dashboard.js
@@ -0,0 +1,179 @@
+import axios from 'axios';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
+import NextLink from 'next/link';
+import React, { useEffect, useContext, useReducer } from 'react';
+import {
+  CircularProgress,
+  Grid,
+  List,
+  ListItem,
+  Typography,
+  Card,
+  ListItemText,
+  CardContent,
+  CardActions,
+  Button,
+} from '@material-ui/core';
+import { getError } from '../../utils/error';
+import { Store } from '../../utils/Store';
+import Layout from '../../components/Layout';
+import useStyles from '../../utils/styles';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true, error: '' };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, summary: action.payload, error: '' };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      state;
+  }
+}
+
+function AdminDashboard() {
+  const { state } = useContext(Store);
+  const router = useRouter();
+  const classes = useStyles();
+  const { userInfo } = state;
+
+  const [{ loading, error, summary }, dispatch] = useReducer(reducer, {
+    loading: true,
+    summary: { salesData: [] },
+    error: '',
+  });
+
+  useEffect(() => {
+    if (!userInfo) {
+      router.push('/login');
+    }
+    const fetchOrders = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get(`/api/admin/summary`, {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        });
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
+    fetchOrders();
+  }, []);
+
+  return (
+    <Layout title="Order History">
+      <Grid container spacing={1}>
+        <Grid item md={3} xs={12}>
+          <Card className={classes.section}>
+            <List>
+              <NextLink href="/admin/dashboard" passHref>
+                <ListItem button component="a">
+                  <ListItemText primary="Admin Dashboard"></ListItemText>
+                </ListItem>
+              </NextLink>
+              <NextLink href="/admin/orders" passHref>
+                <ListItem selected button component="a">
+                  <ListItemText primary="Orders"></ListItemText>
+                </ListItem>
+              </NextLink>
+            </List>
+          </Card>
+        </Grid>
+        <Grid item md={9} xs={12}>
+          <Card className={classes.section}>
+            <List>
+              <ListItem>
+                {loading ? (
+                  <CircularProgress />
+                ) : error ? (
+                  <Typography className={classes.error}>{error}</Typography>
+                ) : (
+                  <Grid container spacing={5}>
+                    <Grid item md={3}>
+                      <Card raised>
+                        <CardContent>
+                          <Typography variant="h1">{summary.ordersPrice}</Typography>
+                          <Typography variant="h1">Sales</Typography>
+                        </CardContent>
+                        <CardActions>
+                          <NextLink href="/admin/orders" passHref>
+                            <Button size='small' color="primary">
+                              View sales
+                            </Button>
+                          </NextLink>
+                        </CardActions>
+                      </Card>
+                    </Grid>
+                    <Grid item md={3}>
+                      <Card raised>
+                        <CardContent>
+                          <Typography variant="h1">{summary.ordersCount}</Typography>
+                          <Typography variant="h1">Orders</Typography>
+                        </CardContent>
+                        <CardActions>
+                          <NextLink href="/admin/orders" passHref>
+                            <Button size='small' color="primary">
+                              View orders
+                            </Button>
+                          </NextLink>
+                        </CardActions>
+                      </Card>
+                    </Grid>
+                    <Grid item md={3}>
+                      <Card raised>
+                        <CardContent>
+                          <Typography variant="h1">{summary.productsCount}</Typography>
+                          <Typography variant="h1">Products</Typography>
+                        </CardContent>
+                        <CardActions>
+                          <NextLink href="/admin/products" passHref>
+                            <Button size='small' color="primary">
+                              View products
+                            </Button>
+                          </NextLink>
+                        </CardActions>
+                      </Card>
+                    </Grid>
+                    <Grid item md={3}>
+                      <Card raised>
+                        <CardContent>
+                          <Typography variant="h1">{summary.usersCount}</Typography>
+                          <Typography variant="h1">Users</Typography>
+                        </CardContent>
+                        <CardActions>
+                          <NextLink href="/admin/users" passHref>
+                            <Button size='small' color="primary">
+                              View users
+                            </Button>
+                          </NextLink>
+                        </CardActions>
+                      </Card>
+                    </Grid>
+                  </Grid>
+                )}
+              </ListItem>
+              <ListItem>
+                <Typography variant="h1" component="h1">
+                  Sales Chart
+                </Typography>
+              </ListItem>
+              <ListItem>
+                Implement sales chart
+              </ListItem>
+              <ListItem>
+                <Typography variant="h1" component="h1">
+                  Sales Chart
+                </Typography>
+              </ListItem>
+            </List>
+          </Card>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
+}
+
+export default dynamic(() => Promise.resolve(AdminDashboard), { ssr: false });
