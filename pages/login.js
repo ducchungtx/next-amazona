5f2dab1947ee0ddd103af33a2788e1a30c77ce54
Chung Nguyen <zrmedia9@gmail.com>
2022-01-07 08:58:45 +0700

    Done to fix error cookies set value.

--- pages/login.js
+++ pages/login.js
@@ -9,7 +9,7 @@ import {
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import NextLink from 'next/link';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import Layout from '../components/Layout';
 import { Store } from '../utils/Store';
 import useStyles from '../utils/styles';
@@ -44,7 +44,7 @@ export default function Login() {
         password,
       });
       dispatch({ type: 'USER_LOGIN', payload: data });
-      Cookies.set('userInfo', data);
+      Cookies.set('userInfo', JSON.stringify(data));
       router.push(redirect || '/');
     } catch (err) {
       enqueueSnackbar(getError(err), { variant: 'error' });
